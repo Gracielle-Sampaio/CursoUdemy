3ed27ac405582ae159940c4611ef846a3890af44
GracielleSampaio <bysunshinearts@gmail.com>
2022-06-27 14:50:12 -0300

    20 aulas e o 1º Projeto

---
+++ Aulas/javascript.js
@@ -0,0 +1,26 @@
+/*var resposta = confirm("Deseja excluir?");
+console.log(resposta);*/
+
+const a =10;
+const b =20;
+
+function somar(a,b){
+  return a+b;
+}
+
+//console.log(somar(10,18));
+
+document.querySelector("#calcular").addEventListener("click", function () {
+  let valorA = document.querySelector("#valorA").value;
+  let valorB = document.querySelector("#valorB").value;
+
+  if(valorA.length > 0 && valorB.length > 0 ){
+
+    alert(parseInt(valorA)+ parseInt(valorB));
+
+  }else{
+    alert("Digite um número");
+  }
+
+
+});
